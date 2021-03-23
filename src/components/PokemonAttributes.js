863a30031708d270beca84503f98483ed727ac87
Erick Enriquez <erick.enriquez10.95@gmail.com>
2021-03-22 18:26:45 -0700

    Added stats section as well as starting the attributes section

--- src/components/PokemonAttributes.js
+++ src/components/PokemonAttributes.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
 class PokemonAttributes extends React.Component {
-	render () {
 
-		const attributes = this.props.activePokemon ? this.props.activePokemon.name : 'no name'
+	render () {
+		const activePokemon = this.props.activePokemon ? this.props.activePokemon : null
+		const height = activePokemon ? ` height ${(activePokemon.height) / 10} m` : null
+		const weight = activePokemon ? ` weight ${(activePokemon.weight) / 10} kg` : null
+		const types = activePokemon ? activePokemon.types.map((item, index) => {
+			return (
+				<div key={index} className={item.type.name} >
+					{item.type.name}
+				</div>
+			)
+		}) : null
 		return (
 			< div className="pokemon-attributes" >
-				{attributes}
+				<p>{height}</p>
+				<p>{weight}</p>
+				{types}
 			</div>
 		)
 	}
