a70d20693647af8b9856e8a4b83acae7a64bb3c1
Erick Enriquez <erick.enriquez10.95@gmail.com>
2021-03-31 14:00:44 -0700

    added react-loading and loading screen on opening section

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,21 @@ import List from './List.js'
 import Details from './Details.js'
 import '../styles/layout.scss'
 import { getKantoPokemon } from '../api/getKantoPokemon'
+import ReactLoading from 'react-loading'
 class App extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = { pokemonList: [], activePokemon: null }
+		this.state = {
+			pokemonList: [],
+			activePokemon: null,
+			isLoading: true
+		}
 		this.getSelectedPokemon = this.getSelectedPokemon.bind(this)
 	}
 
 	async componentDidMount () {
 		let res = await getKantoPokemon()
-		this.setState({ pokemonList: res })
+		this.setState({ pokemonList: res, isLoading: false })
 	}
 
 	getSelectedPokemon (index) {
@@ -23,8 +28,17 @@ class App extends React.Component {
 	render () {
 		return (
 			<main className="container">
-				<List pokemonList={this.state.pokemonList} getSelectedPokemon={this.getSelectedPokemon} />
-				<Details activePokemon={this.state.activePokemon} getSelectedPokemon={this.getSelectedPokemon} pokemonList={this.state.pokemonList} />
+				{ this.state.isLoading ? (
+					<div className="loading-bars">
+						<ReactLoading type={"bubbles"} color={"white"} />
+					</div>
+				) :
+					(
+						<>
+							<List pokemonList={this.state.pokemonList} getSelectedPokemon={this.getSelectedPokemon} />
+							<Details activePokemon={this.state.activePokemon} getSelectedPokemon={this.getSelectedPokemon} pokemonList={this.state.pokemonList} />
+						</>
+					)}
 			</main>
 		)
 	}
