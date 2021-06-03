f4bc0e0ffdf652a8c937a576b2fe6fbfb0a8e455
Erick Enriquez <erick.enriquez10.95@gmail.com>
2021-06-03 10:03:35 -0700

    need to get the d-pad to not be over the list

--- src/components/UserInput/Gamepad.js
+++ src/components/UserInput/Gamepad.js
@@ -3,15 +3,15 @@ import React from 'react'
 class Gamepad extends React.Component {
 	render () {
 		return (
-			<section>
-				<div className="vertical-line">
-					<div className="arrow-up">
-
-					</div>
-				</div>
-			</section>
+			<div class="set">
+				<nav class="d-pad">
+					<a class="up" href="#"></a>
+					<a class="right" href="#"></a>
+					<a class="down" href="#"></a>
+					<a class="left" href="#"></a>
+				</nav>
+			</div>
 		)
 	}
 }
-
 export default Gamepad
